8cb8fe566517f0198c1b973957a3c8deccfda084
Zheka6285 <zhekadee689334@gmail.com>
2019-10-27 12:59:39 +0200

    First commit

---
+++ ConsoleApplication13/ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	float h;
+	cout<<"Enter a =";
+	cin>>a;
+	cout<<"Enter b =";
+	cin>>b;
+	cout<<"Enter h =";
+	cin>>h;
+	
+	float y, x = a;
+	do
+	{
+		y =log(pow(x,2))+2*cos(x)-2;
+		printf("x=%f   y=%f\n", x, y);
+		x += h;
+	} while (x <= b + h / 2);
+
+	
+	system("pause");
+	return 0;
+}
